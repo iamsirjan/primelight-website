f767e1b622e0a7b3c4d00cd38996fe68ca6c0a21
Sirjan Baral <srijan.baral@codehimalaya.net>
2024-08-29 18:05:06 +0545

    feat: theme setup

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,9 +1,12 @@
 import { extendTheme } from '@chakra-ui/react';
 import { THEME_COLORS } from '@shangrila-cargo/theme/colors';
+import { DatePickerTheme } from '@shangrila-cargo/theme/DatePicker.theme';
 
 const theme = extendTheme({
   colors: THEME_COLORS,
-  components: {},
+  components: {
+    DatePicker: DatePickerTheme,
+  },
 });
 
 export default theme;
