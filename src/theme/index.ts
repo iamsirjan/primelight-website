cdd6187c796bbf3a28105a923b66fdc708127404
Sirjan Baral <srijan.baral@codehimalaya.net>
2024-08-29 18:03:12 +0545

    feat: theme setup

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,1 +1,9 @@
-export default {};
+import { extendTheme } from '@chakra-ui/react';
+import { THEME_COLORS } from '@shangrila-cargo/theme/colors';
+
+const theme = extendTheme({
+  colors: THEME_COLORS,
+  components: {},
+});
+
+export default theme;
